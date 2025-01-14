196fb624128ed2e33e72cfd1d51a480997da5466
Tufail Ansari <thebinaryblasters@gmail.com>
2025-01-15 01:41:08 +0530

    almost navbar solved

--- JS/Nav.js
+++ JS/Nav.js
@@ -1,13 +1,20 @@
-const hamburger = document.getElementById('hamburger');
-    const navbarNav = document.getElementById('navbarNav');
+// const hamburger = document.getElementById('hamburger');
+//     const navbarNav = document.getElementById('navbarNav');
 
-    hamburger.addEventListener('click', () => {
-      navbarNav.classList.toggle('active');
-      removeNav();
-    });
+//     hamburger.addEventListener('click', () => {
+//       navbarNav.classList.toggle('active');
+//       removeNav();
+//     });
 
-    const removeNav=()=>{
-      setTimeout(() => {
-        navbarNav.classList.remove('active')
-      }, 1.5*1000);
-    }
+//     const removeNav=()=>{
+//       setTimeout(() => {
+//         navbarNav.classList.remove('active')
+//       }, 1.5*1000);
+//     }
+const toggleButton = document.getElementById('navbar-toggler');
+const navLinks = document.getElementById('nav-links');
+
+// Add event listener for toggling the menu
+toggleButton.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
