32ad0cb8bb45afd49924fc18f3262d1d580ddcf3
Arpit Ranjan <thebinaryblasters@gmail.com>
2025-01-15 21:15:41 +0530

    Updates

--- JS/Nav.js
+++ JS/Nav.js
@@ -11,10 +11,10 @@
 //         navbarNav.classList.remove('active')
 //       }, 1.5*1000);
 //     }
-const toggleButton = document.getElementById('navbar-toggler');
-const navLinks = document.getElementById('nav-links');
+//const toggleButton = document.getElementById('navbar-toggler');
+//const navLinks = document.getElementById('nav-links');
 
 // Add event listener for toggling the menu
-toggleButton.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
+//toggleButton.addEventListener('click', () => {
+  //navLinks.classList.toggle('active');
+//});
